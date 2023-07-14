a1eee826535b598262cec2d537200ac775818ac1
kcminchakpu <min22002@byui.edu>
2023-07-14 21:34:53 +0100

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,18 @@
 /*JS for Last Modified date */
 
 document.getElementById("year").innerHTML = (new Date().toLocaleString());
+
+/* JS for Local Storage */
+
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+      document.getElementById("result").innerHTML = "You have submitted " + localStorage.clickcount + " specialty drinks.";
+    } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }
